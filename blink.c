e255c94578794f25cdff8730085ee480709986cd
Kun Luo <luokun485@gmail.com>
2022-08-06 15:59:26 +0800

    .

--- blink.c
+++ blink.c
@@ -1,16 +1,23 @@
+/*
+ * @Data  : 2022/08/06
+ * @File  : blink.c
+ * @Author: Kun Luo
+ * @Email : luokun485@gmail.com
+ */
+
 #include "blink.h"
 
 #include <stdbool.h>
 
 #include "bsp/board.h"
 
-static uint32_t _blink_interval  = BLINK_NOT_MOUNTED;
-static bool     _blink_state     = false;
-static uint32_t _blink_last_time = 0;
+static uint32_t _interval    = BLINK_NOT_MOUNTED;
+static uint32_t _last_time   = 0;
+static bool     _blink_state = false;
 
 void led_blink_task() {
     uint32_t now = board_millis();
-    if (now - _blink_last_time < _blink_interval) return;
+    if (now - _last_time < _interval) return;
 
     /*  Blink every interval ms */
     board_led_write(_blink_state);
@@ -18,13 +25,13 @@ void led_blink_task() {
     /* toggle */
     _blink_state = !_blink_state;
 
-    _blink_last_time = now;
+    _last_time = now;
 }
 
 void blink_set_pattern(enum blink_pattern pattern) {
-    _blink_interval = pattern;
+    _interval = pattern;
 }
 
 enum blink_pattern blink_get_pattern() {
-    return _blink_interval;
+    return _interval;
 }
